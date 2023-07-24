f39f3c24879ba9ee5c24701cb2f0360893fbc866
natalie <natalie.whalen@marmon.com>
2023-07-24 11:28:22 -0500

    shift & deploy

---
+++ src/Components/Components.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import Home from "./Home/Home.js";
+import Projects from "./Projects/Projects.js";
+import Music from "./Music/Music.js";
+import Art from "./Art/Art.js";
+
+const Components = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/nat" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/music" element={<Music />} />
+        <Route path="/art" element={<Art />} />
+        <Route path="*" element={<Navigate to="/nat" replace />} />
+      </Routes>
+    </Router>
+  );
+};
+
+
+/*
+ <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </Router>
+    */
+/*
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+<Router>
+      <Routes>
+        <Route path="/auth" element={<AuthModule />} />
+        <Route path="/register" element={<AuthRegister />} />
+        <Route path="/login" element={<AuthLogin />} />
+        <Route path="/events" element={<Home />} />
+        <Route path="/events/:locationChoice" element={<Event/>} />
+        <Route path="*" element={<Navigate to="/auth" replace />} />
+      </Routes>
+    </Router>
+*/
+
+export default Components;
+
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import './Nav.css';
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <nav>
+        <ul className="tab-row">
+            <li>
+                <Link to="/nat" className="tab" id="tab1">me</Link>
+            </li>
+            <li>
+                <Link to="/projects" className="tab" id="tab2">projects</Link>
+            </li>
+            <li>
+                <Link to="/music" className="tab" id="tab3">music</Link>
+            </li>
+            <li>
+                <Link to="/art" className="tab" id="tab4">art</Link>
+            </li>
+        </ul>
+      </nav>
+    </div>
+    
+  )
+}
+
+export default Nav;
