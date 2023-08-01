fca8af782a46a003b3d26e20faf92de5a2fa20b9
natalie <natalie.whalen@marmon.com>
2023-08-01 11:36:22 -0500

    highlight page's icon color

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,33 +1,61 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './Nav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Nav = () => {
-  /*
+
+  const iconColors = {
+    natLight: "#ffb457",
+    natDark: "#f88e0b",
+    csLight: "#ff96bd",
+    csDark: "#f85792",
+    musicLight: "#9999fb",
+    musicDark: "#5f5fff",
+    comedyLight: "#59d5e8",
+    comedyDark: "#01b7d3"
+  }
+  let natIconColor = {
+    color: iconColors.natDark
+  };
+  let csIconColor = {
+    color: iconColors.csLight
+  };
+  let musicIconColor = {
+    color: iconColors.musicLight
+  };
+  let comedyIconColor = {
+    color: iconColors.comedyLight
+  };
+
+  // determine which icon should be highlighted
   let path = useLocation().pathname;
-  console.log(path);
-  let background;
   switch(path) {
-    case "/nat":
-      background = "#ffb457";
-      break;
     case "/cs":
-      background = "#ff96bd";
+      natIconColor.color = iconColors.natLight;
+      csIconColor.color = iconColors.csDark;
+      musicIconColor.color = iconColors.musicLight;
+      comedyIconColor.color = iconColors.comedyLight;
       break;
     case "/music":
-      background ="#9999fb";
+      natIconColor.color = iconColors.natLight;
+      csIconColor.color = iconColors.csLight;
+      musicIconColor.color = iconColors.musicDark;
+      comedyIconColor.color = iconColors.comedyLight;
       break;
     case "/comedy":
-      background = "#59e8dad3";
+      natIconColor.color = iconColors.natLight;
+      csIconColor.color = iconColors.csLight;
+      musicIconColor.color = iconColors.musicLight;
+      comedyIconColor.color = iconColors.comedyDark;
       break;
     default:
-      background = "#ffb457";
+      // default * or /nat
+      natIconColor.color = iconColors.natDark;
+      csIconColor.color = iconColors.csLight;
+      musicIconColor.color = iconColors.musicLight;
+      comedyIconColor.color = iconColors.comedyLight;
   }
-  console.log(background);
-  const myBackground = {
-    backgroundColor: background
-  }*/
 
   return (
     <div className="nav" >
@@ -35,22 +63,22 @@ const Nav = () => {
         <ul className="tab-row">
             <li>
                 <Link to="/nat" className="tab" id="tab1">
-                  <FontAwesomeIcon icon="hand" />
+                  <FontAwesomeIcon icon="hand" style={natIconColor}/>
                 </Link>
             </li>
             <li>
                 <Link to="/cs" className="tab" id="tab2">
-                  <FontAwesomeIcon icon="computer" />
+                  <FontAwesomeIcon icon="computer" style={csIconColor}/>
                 </Link>
             </li>
             <li>
                 <Link to="/music" className="tab" id="tab3">
-                  <FontAwesomeIcon icon="music" />
+                  <FontAwesomeIcon icon="music" style={musicIconColor}/>
                 </Link>
             </li>
             <li>
                 <Link to="/comedy" className="tab" id="tab4">
-                  <FontAwesomeIcon icon="masks-theater" />
+                  <FontAwesomeIcon icon="masks-theater" style={comedyIconColor}/>
                 </Link>
             </li>
         </ul>
