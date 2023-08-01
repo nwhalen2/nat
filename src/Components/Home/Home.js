c8bc64ebcee992826e4d6bde057c028e10d92f8e
natalie <natalie.whalen@marmon.com>
2023-08-01 12:23:14 -0500

    image of mwah

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,21 +1,22 @@
 import Nav from "../Nav/Nav.js";
 import "./Home.css";
 import "../../App.css";
+import me from "./nat-orange.png";
 
 const Home = () => {
-    /*const myBackground = {
-        backgroundColor: "#ffb457"
-    }
-    inline style={myBackgound}*/
     return (
-        <div className="homeBody" >
+        <div className="home-body" >
             <Nav />
             <div className="home">
                 <h2>WORK IN PROGRESS</h2>
-                <div className="description-box">
+                <div className="home-box">
                     <span className="description">my name is natalie whalen, & this will be my personal website / portfolio / showcase.
-                    <br/><b>last updated: 1 August 2023</b>
+                    <br/>
                     </span>
+                    <br/>
+                    <img src={me} alt="none" style={{width:"40%"}}/>
+                    <br/>
+                    <b>last updated: 1 August 2023</b>
                 </div>
             </div>
         </div>
