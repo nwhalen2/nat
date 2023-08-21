a210b4d36c91341d56167fe43c637b01edc64c59
natalie <natalie.whalen@marmon.com>
2023-08-22 00:40:35 +0100

    update description

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,13 +10,21 @@ const Home = () => {
             <div className="home">
                 <h2>WORK IN PROGRESS</h2>
                 <div className="home-box">
-                    <span className="description">my name is natalie whalen, & this will be my personal website / portfolio / showcase.
+                    <span className="description">my name is natalie whalen, & this is my personal website. it is very much so still a work in progress. 
+                    <br/>
+                    </span>
+                    <span className="description">everything you see here has been coded in <b>react</b> and is hosted on <b><a href="https://github.com/nwhalen2" target="_blank" rel="noreferrer">github</a></b>
                     <br/>
                     </span>
                     <br/>
                     <img src={me} alt="none" style={{width:"40%"}}/>
                     <br/>
-                    <b>last updated: 3 August 2023</b>
+                    <br/>
+                    <span className="description">i am currently traveling and am hence not actively updating the site. steps forward will be to showcase more coding projects and aspects of my layperson life by making the site more clean and interative. feel free to browse through my current tabs and learn a bit about me!
+                    <br/>
+                    </span>
+                    <br/>
+                    <b>last updated: 22 August 2023</b>
                 </div>
             </div>
         </div>
