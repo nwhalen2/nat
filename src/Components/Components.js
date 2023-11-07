25aa65429acfcb75be494bc14ea365f419f4f596
natalie <natalie.whalen@marmon.com>
2023-11-06 23:32:24 -0600

    clean up

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -24,35 +24,5 @@ const Components = () => {
   );
 };
 
-
-/*
- <Router>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="*" element={<Navigate to="/home" replace />} />
-      </Routes>
-    </Router>
-    */
-/*
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
-
-<Router>
-      <Routes>
-        <Route path="/auth" element={<AuthModule />} />
-        <Route path="/register" element={<AuthRegister />} />
-        <Route path="/login" element={<AuthLogin />} />
-        <Route path="/events" element={<Home />} />
-        <Route path="/events/:locationChoice" element={<Event/>} />
-        <Route path="*" element={<Navigate to="/auth" replace />} />
-      </Routes>
-    </Router>
-*/
-
 export default Components;
 
